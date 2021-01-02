6a2f5e75dbdf33537b3040166ffae7770240a349
Kim <skydevilk@gmail.com>
2021-01-02 12:44:18 +0900

    [feat] HomeMusicListTableView 구현

--- MusicPlayer/MusicPlayer/Controller/HomeViewController.swift
+++ MusicPlayer/MusicPlayer/Controller/HomeViewController.swift
@@ -14,9 +14,25 @@ class HomeViewController: UIViewController {
     }
     @IBAction func randomPlayButtonAction(_ sender: UIButton) {
     }
+    @IBOutlet weak var musicListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        musicListTableView.register(UINib(nibName: HomeMusicListTableViewCell.identifier, bundle: .main), forCellReuseIdentifier: HomeMusicListTableViewCell.identifier)
         playButton.layer.cornerRadius = 10
         randomPlayButton.layer.cornerRadius = 10
+        musicListTableView.delegate = self
+        musicListTableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeMusicListTableViewCell.identifier, for: indexPath) as? HomeMusicListTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
