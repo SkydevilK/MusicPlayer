58d60d2ef1a9b200f6925fd69040eb795d76c2f3
Kim <skydevilk@gmail.com>
2021-01-02 09:19:47 +0900

    [feat] 프로젝트 설정

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 김병인 on 2021/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
